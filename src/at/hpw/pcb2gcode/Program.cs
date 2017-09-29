7caa1947d95ca3730d89eb3f2c35e23bada31ab8
weidingerhp <hans-peter@hp-weidinger.at>
2017-09-29 23:22:36 +0200

    fixed some minor bugs

--- src/at/hpw/pcb2gcode/Program.cs
+++ src/at/hpw/pcb2gcode/Program.cs
@@ -1,14 +1,29 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 using at.hpw.pcb2gcode;
 
 namespace at.hpw.pcb2gcode {
 	class Program {
 		static void Main(string[] args) {
-			var iFile = System.IO.File.OpenText(args[0]);
-			var oFile = System.IO.File.CreateText(args[1]);
-
 			HpglConverter converter = new HpglConverter();
-			converter.ConvertHpgl(iFile, oFile);
+			using (var oFile = System.IO.File.CreateText(args[1])) {
+				using (var iFile = System.IO.File.OpenText(args[0])) {
+
+
+					oFile.Write("G91\nG21\nG1 F1000\n"); // write some init (relative, metric, travel speed)
+
+					converter.ConvertHpgl(iFile, oFile);
+				}
+
+				String input = "PU;\nPA 0,0;\n"; // reset to initial position
+				MemoryStream inputStream = new MemoryStream(Encoding.UTF8.GetBytes(input));
+				using (StreamReader rdr = new StreamReader(inputStream)) {
+					converter.ConvertHpgl(rdr, oFile);
+				}
+				inputStream.Dispose();
+					
+			}
 		}
 	}
 }
