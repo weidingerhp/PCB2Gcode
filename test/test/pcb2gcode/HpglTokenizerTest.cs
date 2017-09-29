33d399be59d58fa6260b61da89799287a4dcbf2c
weidingerhp <hans-peter@hp-weidinger.at>
2017-09-30 00:14:48 +0200

    added some more small tests

--- test/test/pcb2gcode/HpglTokenizerTest.cs
+++ test/test/pcb2gcode/HpglTokenizerTest.cs
@@ -21,6 +21,40 @@ namespace test.pcb2gcode {
 			Assert.Equal(HpglToken.SEMICOLON, (HpglToken) tokenizer.readNextToken());
 			Assert.Equal(HpglToken.PD, (HpglToken) tokenizer.readNextToken());
 			Assert.Equal(HpglToken.SEMICOLON, (HpglToken) tokenizer.readNextToken());
+			Assert.Equal(HpglToken.EOF, (HpglToken) tokenizer.readNextToken());
+		}
+
+		[Fact]
+		void testReadSimpleINIPSC() {
+			String input = "IN; IP 0,0,100,10.03; SC 0,100,0,-100;";
+			MemoryStream inputStream = new MemoryStream(Encoding.UTF8.GetBytes(input));
+			MemoryStream outputStream = new MemoryStream();
+			StreamReader rdr = new StreamReader(inputStream);
+			StreamWriter writer = new StreamWriter(outputStream);
+
+			HpglTokenizer tokenizer = new HpglTokenizer(rdr);
+
+			Assert.Equal(HpglToken.IN, (HpglToken) tokenizer.readNextToken());
+			Assert.Equal(HpglToken.SEMICOLON, (HpglToken) tokenizer.readNextToken());
+			Assert.Equal(HpglToken.IP, (HpglToken) tokenizer.readNextToken());
+			Assert.Equal((double) 0, tokenizer.readNextToken());
+			Assert.Equal(HpglToken.COMMA, (HpglToken) tokenizer.readNextToken());
+			Assert.Equal((double) 0, tokenizer.readNextToken());
+			Assert.Equal(HpglToken.COMMA, (HpglToken) tokenizer.readNextToken());
+			Assert.Equal((double) 100, tokenizer.readNextToken());
+			Assert.Equal(HpglToken.COMMA, (HpglToken) tokenizer.readNextToken());
+			Assert.Equal((double) 10.03, tokenizer.readNextToken());
+			Assert.Equal(HpglToken.SEMICOLON, (HpglToken) tokenizer.readNextToken());
+			Assert.Equal(HpglToken.SC, (HpglToken) tokenizer.readNextToken());
+			Assert.Equal((double) 0, tokenizer.readNextToken());
+			Assert.Equal(HpglToken.COMMA, (HpglToken) tokenizer.readNextToken());
+			Assert.Equal((double) 100, tokenizer.readNextToken());
+			Assert.Equal(HpglToken.COMMA, (HpglToken) tokenizer.readNextToken());
+			Assert.Equal((double) 0, tokenizer.readNextToken());
+			Assert.Equal(HpglToken.COMMA, (HpglToken) tokenizer.readNextToken());
+			Assert.Equal((double) -100, tokenizer.readNextToken());
+			Assert.Equal(HpglToken.SEMICOLON, (HpglToken) tokenizer.readNextToken());
+			Assert.Equal(HpglToken.EOF, (HpglToken) tokenizer.readNextToken());
 		}
 	}
 }
