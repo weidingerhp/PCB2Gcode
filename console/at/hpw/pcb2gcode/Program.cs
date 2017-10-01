e383696d9f43d811364bb01749e845b00ab713d0
weidingerhp <hans-peter@hp-weidinger.at>
2017-10-01 23:53:53 +0200

    using Command-line-parsing lib for console app

--- console/at/hpw/pcb2gcode/Program.cs
+++ console/at/hpw/pcb2gcode/Program.cs
@@ -2,30 +2,48 @@
 using System.IO;
 using System.Text;
 using at.hpw.pcb2gcode;
+using Microsoft.Extensions.CommandLineUtils;
 
 namespace at.hpw.pcb2gcode {
 	class Program {
 		static void Main(string[] args) {
-			HpglConverter converter = new HpglConverter();
+
+            // parse input
+            CommandLineApplication commandLineApplication = new CommandLineApplication(throwOnUnexpectedArg: true);
+            var inputOption = commandLineApplication.Argument(
+				"input <input.hpgl>", 
+				"Input HPGL File to be processed.");
+            var outputOption = commandLineApplication.Argument(
+				"output <output.gcode>", 
+				"Output GCODE File to be written.");
+
+			commandLineApplication.Execute(args);
+
+			if (inputOption.Value == null || outputOption.Value == null) {
+				commandLineApplication.ShowHelp();
+				return;
+			}
+
+            var converter = new HpglConverter();
+			DoConvert(converter, inputOption.Value, outputOption.Value);
+		}
+
+        private static void DoConvert(HpglConverter converter, string inputFile, string outputFile)
+        {
 			converter.InitPosition0 = true;
 
-			using (var oFile = System.IO.File.CreateText(args[1])) {
-				using (var iFile = System.IO.File.OpenText(args[0])) {
+			Console.Out.WriteLine("Converting HPGL '{0}' to GCODE '{1}'", inputFile, outputFile);
 
-
-					oFile.Write("G91\nG21\nG1 F1000\n"); // write some init (relative, metric, travel speed)
+            using (var oFile = System.IO.File.CreateText(outputFile))
+            {
+                using (var iFile = System.IO.File.OpenText(inputFile))
+                {
+                    oFile.Write("G91\nG21\nG1 F1000\n"); // write some init (relative, metric, travel speed)
 
 					converter.ConvertHpgl(iFile, oFile);
-				}
-
-				//String input = "PU;\nPA 0,0;\n"; // reset to initial position
-				//MemoryStream inputStream = new MemoryStream(Encoding.UTF8.GetBytes(input));
-				//using (StreamReader rdr = new StreamReader(inputStream)) {
-				//	converter.ConvertHpgl(rdr, oFile);
-				//}
-				//inputStream.Dispose();
-					
+				}					
 			}
-		}
-	}
+			Console.Out.WriteLine("Finished Converting...");
+        }
+    }
 }
